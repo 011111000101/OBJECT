3a9032e4ed1c20d3b6f2623d0bf49d90f5ed9eae
011111000101 <87422590+011111000101@users.noreply.github.com>
2022-02-10 12:52:26 +0100

    1.0

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("This is JavaScript");
+
+let colors = {
+    color: ["blue", "green", "yellow"],
+    
+    };
+
+colors.hasManyColors = colors.color.length > 3;
+
+console.log(colors.hasManyColors);
